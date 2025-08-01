a12fda6a0342d85810c0b56ed3f5d75d2048c6a3
Rene Ntumngia <rene.ntumngia.ctr@ustaxcourt.gov>
2025-08-01 18:09:48 -0400

    Initial Wagtail blog setup

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,42 @@
 from django.db import models
+from django import forms
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-
-# add this:
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
+from wagtail.snippets.models import register_snippet
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    icon = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
+
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
 
 class BlogIndexPage(Page):
@@ -17,7 +50,9 @@ class BlogIndexPage(Page):
         context['blogpages'] = blogpages
         return context
 
-    content_panels = Page.content_panels + ["intro"]
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+    ]
 
 
 # keep the definition of BlogIndexPage model, and add the BlogPage model:
@@ -26,10 +61,28 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
 
-    content_panels = Page.content_panels + ["date", "intro", "body"]
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel("date"),
+            FieldPanel("tags"),
+            FieldPanel("categories", widget=forms.CheckboxSelectMultiple),
+        ], heading="Blog information"),
+        FieldPanel("intro"),
+        FieldPanel("main_image"),
+        FieldPanel("body"),
+    ]
